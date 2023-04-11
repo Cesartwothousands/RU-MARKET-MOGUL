ecdf3def008a25a0d8f460dd17057f9c0f2555eb
Cesartwothousands <cesarchen616@outlook.com>
2023-04-11 03:45:03 -0400

    DataProcessing

--- Front-end/RUMM_Frontend/src/app/home/home.component.ts
+++ Front-end/RUMM_Frontend/src/app/home/home.component.ts
@@ -12,35 +12,31 @@ export class HomeComponent {
     columnDefs: ColDef[] = [
         { field: 'symbol', sortable: true, headerName: 'Symbol', flex: 1 },
         { field: 'name', sortable: true, headerName: 'Name', flex: 1 },
-        { field: 'livePrice', sortable: true, headerName: 'LivePrice', flex: 1 },
+        { field: 'lastprice', sortable: true, headerName: 'Last Price', flex: 1 },
         {
-            field: 'change',
+            field: 'change1',
             sortable: true,
             headerName: 'Change',
             cellStyle: (params: CellClassParams) => params.value < 0 ? { color: 'red' } : { color: 'green' },
             flex: 1
         },
         {
-            field: 'changePercentage',
+            field: 'change2',
             sortable: true,
-            headerName: 'Change%',
+            headerName: '% Change',
             cellStyle: (params: CellClassParams) => params.value < 0 ? { color: 'red' } : { color: 'green' },
             flex: 1
         },
 
         { field: 'volume', sortable: true, headerName: 'Volume', flex: 1 },
+        { field: 'marketcap', sortable: true, headerName: 'Market Cap', flex: 1 },
+        { field: 'sector', sortable: true, headerName: 'Industry', flex: 1 },
     ];
 
-    rowData = [
-        { symbol: 'AAPL', name: 'Apple Inc.', livePrice: 150, change: 2, changePercentage: 1.3, volume: 100000 },
-        { symbol: 'GOOGL', name: 'Alphabet Inc.', livePrice: 2500, change: -10, changePercentage: -0.4, volume: 50000 },
-        { symbol: 'AMZN', name: 'Amazon.com, Inc.', livePrice: 3300, change: 20, changePercentage: 0.6, volume: 75000 },
-    ];
+    rowData = [{ "symbol": "ATVI", "name": "Activision Blizzard, Inc.", "sector": "Electronic Gaming & Multimedia", "lastprice": 85.48, "change1": 0.26, "change2": 0.31, "volume": 4948951, "marketcap": 67039744000 }, { "symbol": "ADBE", "name": "Adobe Inc.", "sector": "Software\u2014Infrastructure", "lastprice": 376.25, "change1": -4.35, "change2": -1.14, "volume": 1689674, "marketcap": 172585877504 }, { "symbol": "ADP", "name": "Automatic Data Processing, Inc.", "sector": "Staffing & Employment Services", "lastprice": 214.22, "change1": -1.89, "change2": -0.87, "volume": 1076542, "marketcap": 88762482688 },];
 
     rowData2 = [
-        { symbol: 'MSFT', name: 'Microsoft Corporation', livePrice: 290, change: 5, changePercentage: 1.7, volume: 120000 },
-        { symbol: 'TSLA', name: 'Tesla, Inc.', livePrice: 700, change: -15, changePercentage: -2.1, volume: 80000 },
-        { symbol: 'NFLX', name: 'Netflix, Inc.', livePrice: 510, change: 8, changePercentage: 1.6, volume: 90000 },
+        { "symbol": "AMD", "name": "Advanced Micro Devices, Inc.", "sector": "Semiconductors", "lastprice": 84.21, "change1": -0.75, "change2": -0.88, "volume": 30194592, "marketcap": 100997998336 }, { "symbol": "ALGN", "name": "Align Technology, Inc.", "sector": "Medical Devices", "lastprice": 633.5, "change1": -0.5, "change2": -0.08, "volume": 1027940, "marketcap": 51174447104 },
     ];
 
 
